b73cc9ff699c22ea9a3062f4071a585f66c15ee8
Bakhovuddin Latifjanov <salokhiddin2016@Bakhovuddins-MacBook-Pro.local>
2023-02-21 11:54:14 +0500

    Final Task

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int first,ten,hundred;
+        first=number%10;
+        ten = (number/10)%10;
+        hundred=number/100;
+        System.out.println(first*100+ten*10+hundred);
+
     }
 }
